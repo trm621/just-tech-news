c71995e0d12757327d0c8e5f34cdbfe04da5ebce
trm621 <t.maher621@gmail.com>
2022-01-03 17:29:04 -0500

    added ability to login and log out and create cookies

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,7 @@ const { Post, User, Comment, Vote } = require('../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
+  console.log(req.session);
   console.log('======================');
   Post.findAll({
     attributes: [
@@ -37,6 +38,14 @@ router.get('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
 });
 
 module.exports = router;
