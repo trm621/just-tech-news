bd67dc8e39834e40b499b68e97adf07e651e842f
trm621 <t.maher621@gmail.com>
2022-01-03 16:31:44 -0500

    created homepage

--- models/Vote.js
+++ models/Vote.js
@@ -11,19 +11,17 @@ Vote.init(
       autoIncrement: true
     },
     user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'user',
-            key: 'id'
-        }
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
     },
     post_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'post',
-            key: 'id'
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id'
       }
     }
   },
