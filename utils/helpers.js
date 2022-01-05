6a116ae800c4f1ee7da9c940968e978d6892658a
trm621 <t.maher621@gmail.com>
2022-01-05 17:13:14 -0500

    fixed handlebars

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,25 +4,20 @@ module.exports = {
         date
       ).getFullYear()}`;
     },
-
-    format_date: date => {
-      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-        date
-      ).getFullYear()}`;
-    },
-
-    format_plural: (word, amount) => {
-    if (amount !== 1) {
-      return `${word}s`;
-    }
-    return word;
-  },
     format_url: url => {
-        return url
-          .replace('http://', '')
-          .replace('https://', '')
-          .replace('www.', '')
-          .split('/')[0]
-          .split('?')[0];
+      return url
+        .replace('http://', '')
+        .replace('https://', '')
+        .replace('www.', '')
+        .split('/')[0]
+        .split('?')[0];
+    },
+    format_plural: (word, amount) => {
+      if (amount !== 1) {
+        return `${word}s`;
       }
+  
+      return word;
     }
+  };
+  
