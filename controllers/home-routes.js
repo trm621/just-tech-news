02ba58ca0399047ff9cae771e45d6e057bb7e85e
trm621 <t.maher621@gmail.com>
2022-01-05 17:48:37 -0500

    finished app

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -42,14 +42,6 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-  res.render('login');
-});
-
 router.get('/post/:id', (req, res) => {
   Post.findOne({
     where: {
@@ -96,6 +88,14 @@ router.get('/post/:id', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
 });
 
 module.exports = router;
