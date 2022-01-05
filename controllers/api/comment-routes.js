c46dd7ac8ea550a39ef8295a6683f9fd1f6306df
trm621 <t.maher621@gmail.com>
2022-01-05 14:48:54 -0500

    can view single posts

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
+  if (req.session) {
   Comment.create({
     comment_text: req.body.comment_text,
     user_id: req.body.user_id,
@@ -22,6 +22,7 @@ router.post('/', (req, res) => {
       console.log(err);
       res.status(400).json(err);
     });
+  }
 });
 
 router.delete('/:id', (req, res) => {
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -48,6 +48,5 @@ async function loginFormHandler(event) {
     }
   }
   
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
